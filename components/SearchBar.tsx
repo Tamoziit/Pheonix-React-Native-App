79a2f62366fc449169984fadf09770a4ac8caef6
Tamojit Das <tamojitdas181007@gmail.com>
2025-06-07 23:47:47 +0530

    TMDB API custom hook setup

---
+++ components/SearchBar.tsx
@@ -0,0 +1,27 @@
+import { View, Text, Image, TextInput } from 'react-native'
+import React from 'react'
+import { icons } from '@/constants/icons'
+
+interface SearchBarProps {
+	onPress?: () => void;
+	placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: SearchBarProps) => {
+	return (
+		<View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4'>
+			<Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8ff" />
+
+			<TextInput
+				onPress={onPress}
+				placeholder={placeholder}
+				value=""
+				onChangeText={() => { }}
+				placeholderTextColor="#a8bfdb"
+				className='flex-1 ml-2 text-white'
+			/>
+		</View>
+	)
+}
+
+export default SearchBar
